ec00f01df77e868c13b688d8ddf70624387feb4d
root <root@USER.localdomain>
2023-09-14 03:19:26 +0600

    user port input, port checking, reinstallation warning , dowload done

--- sample2.sh
+++ sample2.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Fetch the system's architecture and save it to a variable
+system_architecture=$(dpkg --print-architecture)
+
+# check is port is occpied
 function is_port_occupied() {
     # Get the port number from the user
     port=$1
@@ -15,9 +19,9 @@ function is_port_occupied() {
     # If the port is not in use, return 1
     return 1
 }
-take_input_unitill_right_port() {
-    #!/bin/bash
 
+#take port from user unitll its got right
+function take_port_input_from_user() {
     # Get the port number from the user
     echo "Enter the port number: "
     read port
@@ -32,52 +36,70 @@ take_input_unitill_right_port() {
     return 1
 }
 
-# Fetch the system's architecture and save it to a variable
-system_architecture=$(dpkg --print-architecture)
+function is_already_installed() {
+    # Get the status of the Apache2 service
+    status=$(systemctl is-active apache2.service)
 
-# Download and Install
-
-removeAllreadyExistedFolder() {
-    #!/bin/bash
-
-    # Remove the node_exporter configuration directory
-    sudo rm -r /etc/node_exporter
-
-    # Remove all files and directories named node_exporter
-    sudo rm -r node_exporter*
-
-    # Stop the node_exporter service
-    sudo systemctl stop node_exporter_bgjhsfjhjhsfskdf
-
-    # Print a message
-    echo "after trying to stop"
-
-    # Remove the node_exporter systemd unit file
-    sudo rm -r /etc/systemd/system/node_exporter_limonnnnnnnnnnnnn*
-
-    # Print a message
-    echo "ok"
+    # Return true if the service is active
+    if [[ $status == "active" ]]; then
+        return 0
+    else
+        return 0 ##need to change to 1 false
+    fi
 }
 
-downloadAndInstall() {
-    removeAllreadyExistedFolder
+function remove_allready_existing_files_and_folder() {
+    # Remove the node_exporter configuration directory
+    sudo rm -r /etc/node_exporter
+    # Remove all files and directories named node_exporter
+    sudo rm -r node_exporter*
+    # Stop the node_exporter service
+    sudo systemctl stop node_exporter
+    # Remove the node_exporter systemd unit file
+    sudo rm -r /etc/systemd/system/node_exporter.service
+}
+
+
+
+function download_and_extract_necessary_files_and_folders() {
+    # Display the system's architecture
+    echo "\n System architecture: $system_architecture \n"
     download_link="https://github.com/prometheus/node_exporter/releases/download/v$selected_version/node_exporter-$selected_version.linux-$system_architecture.tar.gz"
     echo 'download link is: '$download_link
+    #download from the github repository
     wget $download_link
     # Extract
     tar xvfz node_exporter-*.*-$system_architecture.tar.gz
-    # save into a variable for future use
-    created_folder=node_exporter-$selected_version.linux-$system_architecture
-    moveDownloadedFile
 }
 
-moveDownloadedFile() {
-
+function move_downloaded_files_and_folders() {
+    #created folder is a combination of  selected version of node_exporter and system_architecture
+    created_folder=node_exporter-$selected_version.linux-$system_architecture
     sudo touch $created_folder/config.yml
     sudo mkdir /etc/node_exporter
     sudo mv $created_folder/node_exporter* /usr/local/bin
-    mv $created_folder /etc/node_exporter
+    mv $created_folder/* /etc/node_exporter
 }
+
+function remove_downloaded_files_and_folders() {
+    #created folder is a combination of  selected version of node_exporter and system_architecture
+    sudo rm -r $created_folder;
+}
+
+
+function removeDownloaded_zip(){
+
+}
+
+
+
+
+
+
+
+
+
+
 # List of available version numbers
 available_versions=("1.3.1" "1.6.1")
 
@@ -89,29 +111,46 @@ done
 
 read -p "Enter the number of the version you want to use: " selected_version_index
 
-# Check if the user input is a valid index
+#Check if the user input is a valid index
+
 if [[ "$selected_version_index" =~ ^[0-9]+$ && "$selected_version_index" -ge 1 && "$selected_version_index" -le "${#available_versions[@]}" ]]; then
     selected_version="${available_versions[selected_version_index - 1]}"
-    # echo "You selected version $selected_version"
-    # download_link="https://github.com/prometheus/node_exporter/releases/download/v$selected_version/node_exporter-$selected_version.linux-$system_architecture.tar.gz"
-    # echo $download_link;
-    # download_link = "https://github.com/prometheus/node_exporter/releases/download/v$selected_version/node_exporter-$selected_version.linux-$system_architecture.tar.gz"
-    # echo 'download link is: '$download_link
+    echo "You selected version is $selected_version "
+    take_port_input_from_user
+    echo "***is node exporter already checking......**"
 
-    downloadAndInstall
-    take_input_unitill_right_port
+    # Start Check if Node Exporter is already installed
+    if is_already_installed; then
+        # Ask the user if they want to remove the existing files and reinstall
+        echo "Node Exporter is already installed. Do you want to remove the existing files and reinstall? (y/n)"
+        read is_reinstall_answer
+        # If the user is_reinstall_answers yes, remove the existing files and reinstall
+        if [[ $is_reinstall_answer == "y" ]]; then
+
+            echo "Removing existing files..."
+            remove_allready_existing_files_and_folder
+            echo "End of Removing existing files..."
+
+            echo "Downloading files..."
+            download_and_extract_necessary_files_and_folders
+            echo "End of downloading files and extract..."
+
+            echo "Moving extracted files..."
+            move_downloaded_files_and_folders
+            echo "End of moving extracted files..."
+
+            echo "Removing unnecessary downloaded files and folder ..."
+            remove_downloaded_raw_files_and_folders
+            echo "Removing downloaded files and folder ..."
+        fi
+    else
+        # Node Exporter is not installed, so do nothing
+        echo "Node Exporter not installed."
+    fi
+    #End of Check if Node Exporter is already installed
+
+    # download_and_extract_necessary_files_and_folders
 else
     echo "Invalid input. Please select a valid version."
     exit 1
 fi
-
-# Display the system's architecture
-echo "System architecture: $system_architecture"
-
-# Now you can use the selected_version in your script
-# For example:
-# if [ "$selected_version" = "3.3.1" ]; then
-#     # Run code for version 3.3.1
-# elif [ "$selected_version" = "3.6.1" ]; then
-#     # Run code for version 3.6.1
-# fi
